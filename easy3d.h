707b31d9113b1db9444480037bdb8517921c3826
mario <mariomac@gmail.com>
2013-12-15 13:27:22 +0100

    laksdjf

--- easy3d.h
+++ easy3d.h
@@ -7,20 +7,37 @@
 #include<GL/glu.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 #define PULSADO 1
 #define SUELTO 0
 
-#define FPS 30.0
+#define ZNEAR 1
+#define ZFAR 200
+#define FOVY 60
 
-#define TECLA_ESC 1
+#define FPS 60.0
+
+#define TECLA_ESC 0b1
+#define TECLA_ARRIBA 2
+#define TECLA_ABAJO 4
+#define TECLA_IZDA 8
+#define TECLA_DCHA 16
 
 typedef struct {
+    double posX,posY,posZ;
+    double anguloX;
+} tcamara;
+
+typedef struct {
+    tcamara cam;
 	int ancho, alto;
 	SDL_Window *wnd;
 	SDL_GLContext glctx;
 	int teclas;
 } tventana;
+
+void cubo(double lado, double cx, double cy, double cz);
 
 tventana ventana;
 
@@ -64,7 +81,10 @@ void abre_ventana() {
     glViewport(0,0, ventana.ancho, ventana.alto);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60.0, (double)ventana.ancho/(double)ventana.alto, 0.1, 10.0);
+    gluPerspective(FOVY, (double)ventana.ancho/(double)ventana.alto, 0.1, ZFAR);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
 
 	//glOrtho(-4, 4, -0.5, 4, 0.1, 1000.0);
 	glMatrixMode(GL_MODELVIEW);
@@ -76,63 +96,24 @@ void cierra_ventana() {
 	SDL_Quit();
 }
 
-double angle = 0;
 void nuevo_fotograma() {
 	SDL_Delay((double)(1000.0/FPS));
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-	gluLookAt(3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+	gluLookAt(ventana.cam.posX, ventana.cam.posY, ventana.cam.posZ,
+           ventana.cam.posX + sin(ventana.cam.anguloX),
+           ventana.cam.posY,
+           ventana.cam.posZ - cos(ventana.cam.anguloX),
+           0,1,0);
 	// Multi-colored side - FRONT
 
-	angle+=1;
-	glRotated(angle,0,0,1);
+    cubo(1, 0, 2, -3);
+    cubo(2, 3, 2, 0);
 
-glBegin(GL_POLYGON);
-glColor3f(   1.0,  1.0, 1.0 );
-glVertex3f(  0.5, -0.5, 0.5 );
-glVertex3f(  0.5,  0.5, 0.5 );
-glVertex3f( -0.5,  0.5, 0.5 );
-glVertex3f( -0.5, -0.5, 0.5 );
-glEnd();
 
-//LADO DERECHO – Lado Morado
-glBegin(GL_POLYGON);
-glColor3f(  1.0,  0.0,  1.0 );
-glVertex3f( 0.5, -0.5, -0.5 );
-glVertex3f( 0.5,  0.5, -0.5 );
-glVertex3f( 0.5,  0.5,  0.5 );
-glVertex3f( 0.5, -0.5,  0.5 );
-glEnd();
 
-//LADO IZQUIERDO – Lado Verde
-glBegin(GL_POLYGON);
-glColor3f(   0.0,  1.0,  0.0 );
-glVertex3f( -0.5, -0.5,  0.5 );
-glVertex3f( -0.5,  0.5,  0.5 );
-glVertex3f( -0.5,  0.5, -0.5 );
-glVertex3f( -0.5, -0.5, -0.5 );
-glEnd();
-
-//LADO SUPERIOR– Lado Azul
-glBegin(GL_POLYGON);
-glColor3f(   0.0,  0.0,  1.0 );
-glVertex3f(  0.5,  0.5,  0.5 );
-glVertex3f(  0.5,  0.5, -0.5 );
-glVertex3f( -0.5,  0.5, -0.5 );
-glVertex3f( -0.5,  0.5,  0.5 );
-glEnd();
-
-//LADO NFERIOR – Lado Rojo
-glBegin(GL_POLYGON);
-glColor3f(   1.0,  0.0,  0.0 );
-glVertex3f(  0.5, -0.5, -0.5 );
-glVertex3f(  0.5, -0.5,  0.5 );
-glVertex3f( -0.5, -0.5,  0.5 );
-glVertex3f( -0.5, -0.5, -0.5 );
-glEnd();
-
-glFlush();
+    glFlush();
 
 
 	SDL_GL_SwapWindow(ventana.wnd);
@@ -144,17 +125,84 @@ glFlush();
 				case SDLK_ESCAPE:
 					ventana.teclas |= TECLA_ESC;
 					break;
+                case SDLK_UP:
+                    ventana.teclas |= TECLA_ARRIBA;
+                    break;
+                case SDLK_DOWN:
+                    ventana.teclas |= TECLA_ABAJO;
+                    break;
+                case SDLK_LEFT:
+                    ventana.teclas |= TECLA_IZDA;
+                    break;
+                case SDLK_RIGHT:
+                    ventana.teclas |= TECLA_DCHA;
+                    break;
 			}
 		} else if(event.type = SDL_KEYUP) {
 			switch(event.key.keysym.sym) {
 				case SDLK_ESCAPE:
 					ventana.teclas &= ~TECLA_ESC;
 					break;
+                case SDLK_UP:
+                    ventana.teclas &= ~TECLA_ARRIBA;
+                    break;
+                case SDLK_DOWN:
+                    ventana.teclas &= ~TECLA_ABAJO;
+                    break;
+                case SDLK_LEFT:
+                    ventana.teclas &= ~TECLA_IZDA;
+                    break;
+                case SDLK_RIGHT:
+                    ventana.teclas &= ~TECLA_DCHA;
+                    break;
 			}
 		}
 	}
+}
 
+void cubo(double lado, double cx, double cy, double cz) {
+    double nx = cx-lado/2.0, px = cx+lado/2.0,
+            ny = cy-lado/2.0, py = cy+lado/2.0,
+            nz = cz-lado/2.0, pz = cz+lado/2.0;
+    glPushMatrix();
+    glTranslated(cx,cy,cz);
+    //frente
+    glBegin(GL_POLYGON);
+        glColor3f(1,0,0);
+        glVertex3f(nx,py,pz);
+        glVertex3f(px,py,pz);
+        glVertex3f(px,ny,pz);
+        glVertex3f(nx,ny,pz);
+    glEnd();
+    //atras
+    glBegin(GL_POLYGON);
+        glColor3f(1,1,1);
+        glVertex3f(nx,ny,nz);
+        glVertex3f(px,ny,nz);
+        glVertex3f(px,py,nz);
+        glVertex3f(nx,py,nz);
+    glEnd();
+    //lado izdo
+    glBegin(GL_POLYGON);
+        glColor3f(0,0,1);
+        glVertex3f(nx,py,nz);
+        glVertex3f(nx,py,pz);
+        glVertex3f(nx,ny,pz);
+        glVertex3f(nx,ny,nz);
+    glEnd();
+    //lado dcho
+    glBegin(GL_POLYGON);
+        glColor3f(1,0,1);
+        glVertex3f(px,ny,nz);
+        glVertex3f(px,ny,pz);
+        glVertex3f(px,py,pz);
+        glVertex3f(px,py,nz);
+    glEnd();
+    glPopMatrix();
+}
 
+void actualiza_camara(tcamara cam) {
+    ventana.cam = cam;
 }
 
 int tecla_pulsada(int tecla) {
--- test.c
+++ test.c
@@ -1,11 +1,30 @@
 #include "easy3d.h"
 
+#define VELOCIDAD 0.25
+#define VROTACION 0.11
+#define ALTURA_OJOS 1.8
+
+
 void main() {
+	tcamara cam = {0, ALTURA_OJOS, 0, 0 };
+
 	abre_ventana();
-	
+
 	while(!tecla_pulsada(TECLA_ESC)) {
+        if(tecla_pulsada(TECLA_IZDA)) {
+            cam.anguloX -= VROTACION;
+        } else if(tecla_pulsada(TECLA_DCHA)) {
+            cam.anguloX += VROTACION;
+        } else if(tecla_pulsada(TECLA_ARRIBA)) {
+           cam.posX += VELOCIDAD * sin(ventana.cam.anguloX);
+           cam.posZ -= VELOCIDAD * cos(ventana.cam.anguloX);
+        } else if(tecla_pulsada(TECLA_ABAJO)) {
+           cam.posX -= VELOCIDAD * sin(ventana.cam.anguloX);
+           cam.posZ += VELOCIDAD * cos(ventana.cam.anguloX);
+        }
+        actualiza_camara(cam);
 		nuevo_fotograma();
 	}
-	
+
 	cierra_ventana();
 }
